b58725873fc583de2f020eeb1a71b112daf8709a
Maxfield Kassel <106034283+MaxfieldKassel@users.noreply.github.com>
2024-10-30 14:30:45 +0000

    Changed build process

--- openssl-fips.nix
+++ openssl-fips.nix
@@ -1,16 +1,15 @@
-{ stdenv, fetchurl, lib, gnumake, gcc, perl, coreutils }:
+{ stdenv, fetchurl, lib, gnumake, gcc, perl, coreutils, patchelf }:
 
 stdenv.mkDerivation rec {
   pname = "openssl-fips";
-  version = "3.0.9";
+  version = "3.0.8"; # Use the FIPS-validated version
 
   src = fetchurl {
     url = "https://www.openssl.org/source/openssl-${version}.tar.gz";
-    sha256 = "6xqwR4FHQ2D3fDGKuJ2MWgOrw45j1lpgPKu/GwCh3JA=";
+    sha256 = "bBPSvzj98x6sPOKjRwc2c/XWMmM5jx9p0N9KQSU+Sz4=";
   };
 
-  # Add required build inputs
-  buildInputs = [ gnumake gcc perl ];
+  buildInputs = [ gnumake gcc perl patchelf ];
 
   configurePhase = ''
     patchShebangs .
@@ -18,21 +17,27 @@ stdenv.mkDerivation rec {
   '';
 
   buildPhase = ''
-    make -j16
+    make -j$NIX_BUILD_CORES
   '';
 
   installPhase = ''
-    make install_sw install_fips
+    make install
+    install -Dm644 LICENSE.txt $out/share/licenses/${pname}/LICENSE.txt
   '';
 
-  postInstall = ''
-    export LD_LIBRARY_PATH=$out/lib64:$out/lib
-    $out/bin/openssl fipsinstall -out $out/etc/ssl/fipsmodule.cnf -module $out/lib64/ossl-modules/fips.so
+  preFixup = ''
+    # Patch binaries to have correct RPATH
+    for bin in $out/bin/*; do
+      patchelf --set-rpath $out/lib:$out/lib64 $bin || true
+    done
   '';
 
   meta = with lib; {
     description = "FIPS-compliant OpenSSL ${version}";
     license = licenses.openssl;
-    platforms = platforms.linux;
+    platforms = platforms.unix;
+    homepage = "https://www.openssl.org";
+    maintainers = with maintainers; [ your-name-here ];
   };
+
 }
