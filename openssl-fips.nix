cc194a0d3d75602aa6067b3217b0f09553d92770
Maxfield Kassel <106034283+MaxfieldKassel@users.noreply.github.com>
2024-11-06 15:56:40 +0000

    Fixed path

--- openssl-fips.nix
+++ openssl-fips.nix
@@ -28,10 +28,9 @@ stdenv.mkDerivation rec {
     echo "Appending custom commands to fixupPhase"
     export LD_LIBRARY_PATH=$out/lib64:$out/lib
     $out/bin/openssl fipsinstall -out $out/etc/ssl/fipsmodule.cnf -module $out/lib64/ossl-modules/fips.so
-
     # Fix the openssl.cnf file to include the fipsmodule.cnf file and enable FIPS mode
     sed -i \
-    -e 's|^# \.include fipsmodule\.cnf|.include $out/etc/ssl/fipsmodule.cnf|' \
+    -e "s|^# \.include fipsmodule\.cnf|.include $out/etc/ssl/fipsmodule.cnf|" \
     -e 's/^# \(fips = fips_sect\)/\1/' \
     -e 's/^\(default = default_sect\)/# \1/' \
     $out/etc/ssl/openssl.cnf
