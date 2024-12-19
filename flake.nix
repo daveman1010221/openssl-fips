f0174e5b83c2f00ce4f8a08a15cbd9ca624f4e7a
David Shepard <daveman1010220@gmail.com>
2024-12-18 20:17:00 -0500

    attempting to fix override error for packages that inherit this one

--- flake.nix
+++ flake.nix
@@ -9,88 +9,96 @@
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      lib = nixpkgs.lib;
     in
     {
       packages = forAllSystems (system:
         let
           pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          # Provide separate outputs for 'out', 'dev', etc.
-          default = pkgs.stdenv.mkDerivation rec {
-            pname = "openssl-fips";
-            version = "3.0.8";
 
-            src = pkgs.fetchurl {
-              url = "https://www.openssl.org/source/openssl-${version}.tar.gz";
-              sha256 = "bBPSvzj98x6sPOKjRwc2c/XWMmM5jx9p0N9KQSU+Sz4=";
-            };
+          # Common logic for FIPS OpenSSL
+          commonFips = { version, sha256 }:
+            pkgs.stdenv.mkDerivation rec {
+              pname = "openssl-fips";
+              inherit version;
 
-            nativeBuildInputs = [ pkgs.perl pkgs.gnumake ];
+              src = pkgs.fetchurl {
+                url = "https://www.openssl.org/source/openssl-${version}.tar.gz";
+                inherit sha256;
+              };
 
-            outputs = [ "out" "dev" "man" ];
+              nativeBuildInputs = [ pkgs.perl pkgs.gnumake ];
 
-            configurePhase = ''
-              patchShebangs .
-              ./Configure enable-fips --prefix=$out --openssldir=$out/etc/ssl
-            '';
+              outputs = [ "bin" "dev" "out" "man" ];
 
-            buildPhase = ''
-              make -j$NIX_BUILD_CORES
-            '';
+              configurePhase = ''
+                patchShebangs .
+                ./Configure enable-fips --prefix=$out --openssldir=$out/etc/ssl
+              '';
 
-            installPhase = ''
-              # Install the libraries
-              make install -j$NIX_BUILD_CORES
-            
+              buildPhase = ''
+                make -j$NIX_BUILD_CORES
+              '';
+              installPhase = ''
+                # Install the libraries
+                make install -j$NIX_BUILD_CORES
+
               # Need to fixup the files so that when installed, the MAC is correct
               # if you do not do this, the FIPS module will not load.
-              runHook fixupPhase
-            
+                runHook fixupPhase
+
               # Set the library path so that the FIPS module can be installed
-              export LD_LIBRARY_PATH=$out/lib64:$out/lib
-            
-              # Install the FIPS module
-              $out/bin/openssl fipsinstall -out $out/etc/ssl/fipsmodule.cnf -module $out/lib64/ossl-modules/fips.so
-            
-              # Fix the openssl.cnf file to include the fipsmodule.cnf file and enable FIPS mode
-              sed -i \
-                -e "s|^# \.include fipsmodule\.cnf|.include $out/etc/ssl/fipsmodule.cnf|" \
-                -e 's/^# \(fips = fips_sect\)/\1/' \
-                -e 's/^\(default = default_sect\)/# \1/' \
-                $out/etc/ssl/openssl.cnf
-            
+                export LD_LIBRARY_PATH=$out/lib64:$out/lib
+
+                # Install the FIPS module
+                $out/bin/openssl fipsinstall -out $out/etc/ssl/fipsmodule.cnf -module $out/lib64/ossl-modules/fips.so
+
+                # Fix the openssl.cnf file to include the fipsmodule.cnf file and enable FIPS mode
+                sed -i \
+                  -e "s|^# \.include fipsmodule\.cnf|.include $out/etc/ssl/fipsmodule.cnf|" \
+                  -e 's/^# \(fips = fips_sect\)/\1/' \
+                  -e 's/^\(default = default_sect\)/# \1/' \
+                  $out/etc/ssl/openssl.cnf
+
               # Create destination directories
-              mkdir -p $dev/include
-              mkdir -p $man/share/man
-            
-              # Check if include files exist before moving
-              if [ -d "$out/include" ] && [ "$(ls -A $out/include)" ]; then
-                mv $out/include/* $dev/include/
-              fi
-            
-              # Check if man pages exist before moving
-              if [ -d "$out/share/man" ] && [ "$(ls -A $out/share/man)" ]; then
-                mv $out/share/man/* $man/share/man/
-              fi
-            
-              # Remove unnecessary documentation
-              rm -rf $out/share/doc
-            '';
+                mkdir -p $dev/include
+                mkdir -p $man/share/man
 
-            postInstall = ''
-              # Ensure pkg-config files are set correctly for FIPS OpenSSL
-              sed -i "s|prefix=.*|prefix=$out|" $dev/lib/pkgconfig/*.pc
-              sed -i "s|exec_prefix=.*|exec_prefix=$out|" $dev/lib/pkgconfig/*.pc
-            '';
+                # Check if include files exist before moving
+                if [ -d "$out/include" ] && [ "$(ls -A $out/include)" ]; then
+                  mv $out/include/* $dev/include/
+                fi
 
-            meta = with pkgs.lib; {
-              description = "FIPS-compliant OpenSSL ${version}";
-              license = licenses.openssl;
-              platforms = platforms.unix;
+                # Check if man pages exist before moving
+                if [ -d "$out/share/man" ] && [ "$(ls -A $out/share/man)" ]; then
+                  mv $out/share/man/* $man/share/man/
+                fi
+            
+                # Remove unnecessary documentation
+                rm -rf $out/share/doc
+              '';
+
+              postInstall = ''
+                # Ensure pkg-config files are set correctly for FIPS OpenSSL
+                sed -i "s|prefix=.*|prefix=$out|" $dev/lib/pkgconfig/*.pc
+                sed -i "s|exec_prefix=.*|exec_prefix=$out|" $dev/lib/pkgconfig/*.pc
+              '';
+
+              meta = with pkgs.lib; {
+                description = "FIPS-compliant OpenSSL ${version}";
+                license = licenses.openssl;
+                platforms = platforms.unix;
+              };
             };
+
+          # Define FIPS versions
+          opensslFips3_0_8 = commonFips {
+            version = "3.0.8";
+            sha256 = "bBPSvzj98x6sPOKjRwc2c/XWMmM5jx9p0N9KQSU+Sz4=";
           };
+        in
+        # Add `override` support directly in the default package
+        opensslFips3_0_8 // {
+          override = attrs: pkgs.stdenv.mkDerivation (opensslFips3_0_8 // attrs);
         }
       );
     };
